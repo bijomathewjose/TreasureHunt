591ac8992eca30c8df3a8a1a44fee75f0cb79313
Amal C P <acp070146@gmail.com>
2024-02-08 20:59:08 +0530

    pages initalised

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import { Home } from "./Pages/Home/Home";
+import NotFound from "./Pages/NotFound/NotFount";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+    {
+      path: "/404",
+      element: <NotFound />,
+    },
+    {
+      path: "/",
+      element: <Navigate to="/home" replace />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+  
+  ]);
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/Pages/Register/Payment.tsx
@@ -0,0 +1,8 @@
+
+type Props = {}
+
+export const Payment = (_props: Props) => {
+  return (
+    <div>Payment</div>
+  )
+}
